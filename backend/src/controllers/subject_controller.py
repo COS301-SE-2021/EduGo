cfe9ad0474a1c4d4411413b9990c50b49cf3ef19
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-06-02 22:18:06 +0200

    added swagger generated flask code

---
+++ backend/src/controllers/subject_controller.py
@@ -0,0 +1,16 @@
+import connexion
+import six
+
+from swagger_server.models.api_response import ApiResponse  # noqa: E501
+from swagger_server import util
+
+
+def create_subject():  # noqa: E501
+    """Create subject for organization
+
+     # noqa: E501
+
+
+    :rtype: ApiResponse
+    """
+    return 'do some magic!'
