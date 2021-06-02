e5ed93c04e4d7e60cab6dda99f01e3565b5a3b8d
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-02 23:22:33 +0200

    Working Backend Server added

--- backend/swagger_server/util.py
+++ backend/swagger_server/util.py
@@ -2,7 +2,6 @@ import datetime
 
 import six
 import typing
-from swagger_server import type_util
 
 
 def _deserialize(data, klass):
@@ -16,7 +15,7 @@ def _deserialize(data, klass):
     if data is None:
         return None
 
-    if klass in six.integer_types or klass in (float, str, bool, bytearray):
+    if klass in six.integer_types or klass in (float, str, bool):
         return _deserialize_primitive(data, klass)
     elif klass == object:
         return _deserialize_object(data)
@@ -24,10 +23,10 @@ def _deserialize(data, klass):
         return deserialize_date(data)
     elif klass == datetime.datetime:
         return deserialize_datetime(data)
-    elif type_util.is_generic(klass):
-        if type_util.is_list(klass):
+    elif type(klass) == typing.GenericMeta:
+        if klass.__extra__ == list:
             return _deserialize_list(data, klass.__args__[0])
-        if type_util.is_dict(klass):
+        if klass.__extra__ == dict:
             return _deserialize_dict(data, klass.__args__[1])
     else:
         return deserialize_model(data, klass)
@@ -52,7 +51,7 @@ def _deserialize_primitive(data, klass):
 
 
 def _deserialize_object(value):
-    """Return an original value.
+    """Return a original value.
 
     :return: object.
     """
