42f5f992777034cdc9c95f586a9c6874d141ce78
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-04 03:32:51 +0200

    Status 200 test for subject

--- backend/test/subject.test.js
+++ backend/test/subject.test.js
@@ -8,7 +8,16 @@ describe("POST /lesson/createLesson", () => {
 
   describe("Subject is created ", () => {
 
-   
+    test("should respond with a 200 status code", async () => {
+        const response = await request(app).post("/subject/createSubject").send({
+          
+          description: "Some Subject description",
+          title:"Edugo Subject",
+           
+        })
+        expect(response.statusCode).toBe(200)
+      })
+    
 
   })
 
