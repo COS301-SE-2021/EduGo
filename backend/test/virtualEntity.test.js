a127d51a480e8ccc8c259e98aea3376c2e8b065e
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-04 03:36:51 +0200

    virtual entity test init

---
+++ backend/test/virtualEntity.test.js
@@ -0,0 +1,13 @@
+//import request from 'supertest'
+//import app from './app.js'
+
+const request = require('supertest')
+const { app } = require('../dist/index')
+
+describe("POST /virtualEntity/createVirtualEntity", () => {
+
+  describe("Virtual Entity is created ", () => {
+
+  })
+
+})
