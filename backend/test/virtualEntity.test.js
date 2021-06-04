2acfca17482932be0e8f32d4389d0c4e1cbc5970
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-04 03:39:14 +0200

    Status 400 test completed Virtual ENtity

--- backend/test/virtualEntity.test.js
+++ backend/test/virtualEntity.test.js
@@ -7,7 +7,16 @@ const { app } = require('../dist/index')
 describe("POST /virtualEntity/createVirtualEntity", () => {
 
   describe("Virtual Entity is created ", () => {
-
+    test("should respond with a 200 status code", async () => {
+        const response = await request(app).post("/subject/createSubject").send({
+          
+          description: "Some Subject description",
+          title:"Edugo Subject",
+           
+        })
+        expect(response.statusCode).toBe(200)
+      })
+  
 
   })
 
