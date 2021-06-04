f465bad0e3601aa94ca845fc7053506da877bb11
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-04 03:40:22 +0200

    Virtual Enity JSON header test

--- backend/test/virtualEntity.test.js
+++ backend/test/virtualEntity.test.js
@@ -16,7 +16,15 @@ describe("POST /virtualEntity/createVirtualEntity", () => {
         })
         expect(response.statusCode).toBe(200)
       })
-  
+      test("should specify json in the content type header", async () => {
+        const response = await request(app).post("/subject/createSubject").send({
+            
+          description: "Some Subject description",
+          title:"Edugo Subject",
+           
+        })
+        expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+      })
 
   })
 
