70e90f3bb2e5b5393fcce1b1e461f3b881bfeb44
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-04 03:42:54 +0200

    Virtual entity test done

--- backend/test/virtualEntity.test.js
+++ backend/test/virtualEntity.test.js
@@ -8,19 +8,19 @@ describe("POST /virtualEntity/createVirtualEntity", () => {
 
   describe("Virtual Entity is created ", () => {
     test("should respond with a 200 status code", async () => {
-        const response = await request(app).post("/subject/createSubject").send({
+        const response = await request(app).post("/virtualEntity/createVirtualEntity").send({
+          description: "Some Virtual Entity description",
+          title:"Edugo Virtual Entity",
           
-          description: "Some Subject description",
-          title:"Edugo Subject",
            
         })
         expect(response.statusCode).toBe(200)
       })
       test("should specify json in the content type header", async () => {
-        const response = await request(app).post("/subject/createSubject").send({
+        const response = await request(app).post("/virtualEntity/createVirtualEntity").send({
             
-          description: "Some Subject description",
-          title:"Edugo Subject",
+          description: "Some Virtual Entity description",
+          title:"Edugo Virtual Entity",
            
         })
         expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
@@ -30,9 +30,9 @@ describe("POST /virtualEntity/createVirtualEntity", () => {
 
   describe("when the one of the parameters are missing", () => {
     test("should respond with a 400 status code", async () => {
-      const response = await request(app).post("/subject/createSubject").send({
+      const response = await request(app).post("/virtualEntity/createVirtualEntity").send({
         
-        title:"Edugo Subject",
+        title:"Edugo Virtual Entity",
          
       })
       expect(response.statusCode).toBe(400)
