5f42d18936bd2f406b878a2702d2270062c5e4e6
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-09 21:09:43 +0200

    Revert "Depricate code"
    This reverts commit 3c773f103adc4f9ff9e52fcd3410fd4ee9885fb4.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import {Client} from 'pg';
 import { dbInit } from './database/index';
-
+import Container from 'typedi';
+import 'reflect-metadata'
 //dotenv.config();
 
 // const client = new Client({
@@ -20,23 +21,6 @@ import { dbInit } from './database/index';
 
 dbInit();
 
-import {router as LessonController} from './lesson/api/controller';
-import {router as SubjectController} from './lesson/api/controller';
-import {router as VirtualEntityController} from './lesson/api/controller';
 
-const PORT = process.env.PORT || 8080;
-
-export const app = express();
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(cors());
-app.use('/lesson', LessonController)
-app.use('/subject', SubjectController)
-
-app.use('/virtualEntity', VirtualEntityController)
-
-app.get('/', (req, res) => {
-    res.send('hey there')
-})
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
