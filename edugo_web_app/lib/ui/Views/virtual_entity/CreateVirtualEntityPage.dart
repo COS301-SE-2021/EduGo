83ea59494236673cab98d4e9600577d8d3291d6f
Noah <u19181851@tuks.co.za>
2021-06-17 00:39:23 +0200

    Creatte Widgets and Virtual Entity UI

--- edugo_web_app/lib/ui/Views/virtual_entity/CreateVirtualEntityPage.dart
+++ edugo_web_app/lib/ui/Views/virtual_entity/CreateVirtualEntityPage.dart
@@ -1,10 +1,49 @@
+import 'package:edugo_web_app/ui/widgets/EduGoContainer.dart';
+import 'package:edugo_web_app/ui/widgets/EduGoPage.dart';
+import 'package:edugo_web_app/ui/widgets/input_fields/EduGoInput.dart';
+import 'package:edugo_web_app/ui/widgets/input_fields/EduGoMultiLineInput.dart';
 import 'package:flutter/material.dart';
 
 class CreateVirtualEntityPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        //Code Goes Here
-        );
+    return EduGoPage(
+      child: Stack(
+        children: <Widget>[
+          EduGoContainer(
+            width: MediaQuery.of(context).size.width - 100,
+            height: MediaQuery.of(context).size.height - 100,
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Align(
+                      alignment: Alignment.topLeft,
+                      child: Text("Create Virtual Entity",
+                          style: TextStyle(fontSize: 25))),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        child: Column(children: <Widget>[
+                          EduGoInput(hintText: "Entity name", width: 450),
+                          SizedBox(
+                            height: 25,
+                          ),
+                          EduGoMultiLineInput(
+                            width: 450,
+                            maxLines: 3,
+                            hintText: "Entity description",
+                          )
+                        ]),
+                      ))
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
