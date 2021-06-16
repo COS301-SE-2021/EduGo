eba48abfaa09874a38571a0dac90e1e17f521942
Noah <u19181851@tuks.co.za>
2021-06-16 15:16:10 +0200

    Renamed files and altered folder structure

---
+++ edugo_web_app/lib/ui/Views/lesson/CreateLessonPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateLessonPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //Code Goes Here
+        );
+  }
+}
