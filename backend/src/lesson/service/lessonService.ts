697b3db36879e7afa75e1c8e17081ed7b894817e
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-17 17:46:01 +0200

    Lesson service added

--- backend/src/lesson/service/lessonService.ts
+++ backend/src/lesson/service/lessonService.ts
@@ -1,27 +1,46 @@
 import { CreateLessonRequest } from '../models/CreateLessonRequest';
+import {  GetLessonResponse} from '../models/GetLessonResponse'
 import { ApiResponse } from '../../models/apiResponse';
 import { Lesson } from '../../database/entity/Lesson';
 import { createConnection } from 'typeorm';
+
 
 export async function createLesson(request: CreateLessonRequest) : Promise<ApiResponse> {
     let response: ApiResponse = {
         message: '',
         type: 'fail'
     }
-    let conn = await createConnection();
-    let lessonRepository = conn.getRepository(Lesson);
-
-    let lesson: Lesson = new Lesson();
-    lesson.title = request.title;
-    lesson.description = request.description;
-
-    return lessonRepository.save(lesson).then(value => {
-        response.message = `Successfully added lesson ${value.id}`
-        response.type = 'success'
-        return response;
-    }).catch(() => {
-        response.message = 'There was an error adding the lesson'
+  
+    if(request.title== null||request.date == null|| request.description == null){
+        response.message = "Missing parameters"
         response.type = 'fail'
-        return response;
-    })
+        return response; 
+    }
+
+    else{
+        let conn = await createConnection();
+        let lessonRepository = conn.getRepository(Lesson);
+        let lesson: Lesson = new Lesson();
+    
+        lesson.title = request.title;
+        lesson.description = request.description;
+        lesson.date = request.date; 
+        return lessonRepository.save(lesson).then(value => {
+            response.message = `Successfully added lesson ${value.id}`
+            response.type = 'success'
+            return response; 
+
+        }).catch(() => {
+            response.message = 'There was an error adding the lesson to the database'
+            response.type = 'fail'
+            return response;
+        })
+
+    }
 }
+
+
+export async function getLesson() : Promise<GetLessonResponse> {
+    
+    
+} 
