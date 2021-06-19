71e8483c8345483fca242b7761acc1c7028d0301
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-20 00:52:27 +0200

    Update jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,14 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+import type {Config} from '@jest/types';
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+};
+export default config;
+
+// Or async function
+export default async (): Promise<Config.InitialOptions> => {
+  return {
+    verbose: true,
+  };
 };
