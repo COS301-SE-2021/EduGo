6396f33726607312558290ecc2cef4cee4ae0353
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-20 00:56:10 +0200

    Revert "Update jest.config.js"
    This reverts commit 71e8483c8345483fca242b7761acc1c7028d0301.

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,14 +1,4 @@
-import type {Config} from '@jest/types';
-
-// Sync object
-const config: Config.InitialOptions = {
-  verbose: true,
-};
-export default config;
-
-// Or async function
-export default async (): Promise<Config.InitialOptions> => {
-  return {
-    verbose: true,
-  };
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
