2ba73cf500825aeb19ea54e9c149ec25ab121fc8
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-20 01:39:06 +0200

    Update jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,17 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "setupFiles": [
+    "<rootDir>/test/setupTests.ts"
+  ]
 };
