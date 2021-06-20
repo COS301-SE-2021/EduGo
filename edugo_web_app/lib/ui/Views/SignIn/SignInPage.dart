a645369df1303c041e7e6149ac256974d33088ae
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-06-20 12:09:25 +0200

    feat: create Layout

--- edugo_web_app/lib/ui/Views/SignIn/SignInPage.dart
+++ edugo_web_app/lib/ui/Views/SignIn/SignInPage.dart
@@ -1,1 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:mish/Views/SignIn/SignInPageContent.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Container(
+                          width: MediaQuery.of(context).size.width / 2,
+                          height: MediaQuery.of(context).size.height,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color.fromRGBO(255, 247, 223, 1.0),
+                                Color.fromRGBO(255, 251, 240, 1.0)
+                              ],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                            ),
+                          ),
+                          child: SignInPageContent())
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                          width: MediaQuery.of(context).size.width / 2,
+                          height: MediaQuery.of(context).size.height,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color.fromRGBO(203, 203, 203, 1.0),
+                                Color.fromRGBO(232, 226, 226, 1.0)
+                              ],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                            ),
+                          ),
+                          child: Text('Column 1'))
+                    ],
+                  ),
+                ])));
+  }
+}
