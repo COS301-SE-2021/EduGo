39beb10b90955a1e0a0a664b25d7f4abb2bb1054
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-06-20 19:04:55 +0200

    feat: add images

--- edugo_web_app/lib/ui/Views/lesson/LessonPage.dart
+++ edugo_web_app/lib/ui/Views/lesson/LessonPage.dart
@@ -1,9 +1,142 @@
+import 'package:edugo_web_app/ui/Views/subject/SubjectsPage.dart';
+import 'package:edugo_web_app/ui/widgets/EduGoContainer.dart';
 import 'package:edugo_web_app/ui/widgets/EduGoPage.dart';
+import 'package:edugo_web_app/ui/widgets/input_fields/EduGoInput.dart';
+import 'package:edugo_web_app/ui/widgets/input_fields/EduGoMultiLineInput.dart';
 import 'package:flutter/material.dart';
 
 class LessonPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return EduGoPage(child: Text("Lesson Page"));
+    return EduGoPage(
+      child: Stack(
+        children: <Widget>[
+          EduGoContainer(
+            width: MediaQuery.of(context).size.width - 100,
+            height: MediaQuery.of(context).size.height - 100,
+            child: Row(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Text("Algebra 101 - Basic Algebra",
+                      style: TextStyle(fontSize: 25)),
+                ),
+                Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 40.0),
+                      child: Image.asset(
+                        "../assets/images/change_title_button.png",
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                    )),
+
+                //
+
+                SizedBox(height: 35),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height - 360,
+                        // decoration: BoxDecoration(
+                        //   borderRadius: BorderRadius.only(
+                        //     topRight: Radius.circular(40),
+                        //     topLeft: Radius.circular(40),
+                        //     bottomLeft: Radius.circular(40),
+                        //     bottomRight: Radius.circular(40),
+                        //   ),
+                        // ),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: <Widget>[
+                              SizedBox(height: 25),
+                              EduGoInput(
+                                  hintText: "Enter the subject name",
+                                  width: 450),
+                              SizedBox(height: 25),
+                              EduGoInput(
+                                  hintText: "Enter the subject grade",
+                                  width: 450),
+                              SizedBox(height: 25),
+                              EduGoMultiLineInput(
+                                  hintText: "Enter the subject description",
+                                  maxLines: 4,
+                                  width: 450),
+                              SizedBox(height: 100),
+                              MaterialButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10))),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => SubjectsPage()),
+                                  );
+                                },
+                                minWidth: 400,
+                                height: 60,
+                                child: Text("Add Subject",
+                                    style: TextStyle(color: Colors.white)),
+                                color: Color.fromARGB(255, 97, 211, 87),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 100),
+                      Container(
+                        height: MediaQuery.of(context).size.height - 450,
+                        //height: 600,
+                        width: 500,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(40),
+                            topLeft: Radius.circular(40),
+                            bottomLeft: Radius.circular(40),
+                            bottomRight: Radius.circular(40),
+                          ),
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 1,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            SizedBox(height: 60),
+                            Icon(
+                              Icons.add_a_photo_outlined,
+                              size: 200,
+                              color: Color.fromARGB(255, 97, 211, 87),
+                            ),
+                            SizedBox(height: 60),
+                            MaterialButton(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              onPressed: () {},
+                              minWidth: 65,
+                              height: 40,
+                              child: Text("Add Photo",
+                                  style: TextStyle(color: Colors.white)),
+                              color: Color.fromARGB(255, 97, 211, 87),
+                              // margin: EdgeInsets.fromLTRB(20, 100, 20, 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
