1183d2dcd9d41746e9e4dc2c31e6193c367c2f94
Noah <u19181851@tuks.co.za>
2021-06-21 00:19:57 +0200

    Create Virtual Enitity Half Done

--- edugo_web_app/lib/ui/Views/virtual_entity/VirtualEntityStorePage.dart
+++ edugo_web_app/lib/ui/Views/virtual_entity/VirtualEntityStorePage.dart
@@ -1,9 +1,24 @@
+import 'package:edugo_web_app/ui/widgets/EduGoContainer.dart';
 import 'package:edugo_web_app/ui/widgets/EduGoPage.dart';
 import 'package:flutter/material.dart';
 
 class VirtualEntityStore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return EduGoPage(child: Text("Virtual Entity Store Page"));
+    return EduGoPage(
+        child: EduGoContainer(
+            width: MediaQuery.of(context).size.width - 100,
+            height: MediaQuery.of(context).size.height - 70,
+            child: Column(
+              children: <Widget>[
+                Align(
+                    alignment: Alignment.topLeft,
+                    child: Text("Create Virtual Entity",
+                        style: TextStyle(fontSize: 25))),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            )));
   }
 }
