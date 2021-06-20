b9ae300539d0d0eaf842cf636bc18cf27138a028
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-06-20 13:08:20 +0200

    feat: update form for consistency

--- edugo_web_app/lib/ui/Views/SignIn/SignInPageForm.dart
+++ edugo_web_app/lib/ui/Views/SignIn/SignInPageForm.dart
@@ -24,86 +24,89 @@ class _SignInPageFormState extends State<SignInPageForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: SingleChildScrollView(
-        child: Form(
-          autovalidateMode: AutovalidateMode.always,
-          key: formkey,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 60.0),
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 40.0),
-                    child: Image.asset(
-                      "../assets/images/edugo_logo.png",
-                      width: MediaQuery.of(context).size.width / 2,
+        backgroundColor: Colors.transparent,
+        body: Stack(children: <Widget>[
+          new Container(color: Color.fromRGBO(255, 247, 223, 1.0)),
+          SingleChildScrollView(
+            child: Form(
+              autovalidateMode: AutovalidateMode.always,
+              key: formkey,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 60.0),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 40.0),
+                        child: Image.asset(
+                          "../assets/images/edugo_logo.png",
+                          width: MediaQuery.of(context).size.width / 2,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 15),
-                child: TextFormField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Email',
-                        hintText: 'Enter valid email id as abc@gmail.com'),
-                    validator: MultiValidator([
-                      RequiredValidator(errorText: "* Required"),
-                      EmailValidator(errorText: "Enter valid email id"),
-                    ])),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 15.0, right: 15.0, top: 15, bottom: 0),
-                child: TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Password',
-                        hintText: 'Enter secure password'),
-                    validator: MultiValidator([
-                      RequiredValidator(errorText: "* Required"),
-                      MinLengthValidator(6,
-                          errorText: "Password should be atleast 6 characters"),
-                      MaxLengthValidator(15,
-                          errorText:
-                              "Password should not be greater than 15 characters")
-                    ])
-                    //validatePassword,        //Function to check validation
-                    ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 50,
-                width: 250,
-                decoration: BoxDecoration(
-                    color: Color.fromRGBO(20, 195, 50, 1.0),
-                    borderRadius: BorderRadius.circular(20)),
-                child: TextButton(
-                  onPressed: () {
-                    if (formkey.currentState!.validate()) {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (_) => SubjectPage()));
-                      print("Validated");
-                    } else {
-                      print("Not Validated");
-                    }
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: TextFormField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Email',
+                            hintText: 'Enter valid email id as abc@gmail.com'),
+                        validator: MultiValidator([
+                          RequiredValidator(errorText: "* Required"),
+                          EmailValidator(errorText: "Enter valid email id"),
+                        ])),
                   ),
-                ),
-              )
-            ],
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 15.0, right: 15.0, top: 15, bottom: 0),
+                    child: TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                            hintText: 'Enter secure password'),
+                        validator: MultiValidator([
+                          RequiredValidator(errorText: "* Required"),
+                          MinLengthValidator(6,
+                              errorText:
+                                  "Password should be atleast 6 characters"),
+                          MaxLengthValidator(15,
+                              errorText:
+                                  "Password should not be greater than 15 characters")
+                        ])
+                        //validatePassword,        //Function to check validation
+                        ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 250,
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(20, 195, 50, 1.0),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: TextButton(
+                      onPressed: () {
+                        if (formkey.currentState!.validate()) {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (_) => SubjectPage()));
+                          print("Validated");
+                        } else {
+                          print("Not Validated");
+                        }
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        ]));
   }
 }
