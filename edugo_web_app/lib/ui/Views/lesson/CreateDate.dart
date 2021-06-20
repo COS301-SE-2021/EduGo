ca6e9fe279b9b84ad03c5180760bfec03b67eb90
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-06-20 16:44:06 +0200

    feat: update date and time

--- edugo_web_app/lib/ui/Views/lesson/CreateDate.dart
+++ edugo_web_app/lib/ui/Views/lesson/CreateDate.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
 
-class CreateDart extends StatelessWidget {
+class CreateDate extends StatefulWidget {
+  @override
+  _CreateDateState createState() => _CreateDateState();
+}
+
+class _CreateDateState extends State<CreateDate> {
+  void _selectDate() async {
+    final DateTime newDate = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: DateTime(2017, 1),
+      lastDate: DateTime(2022, 7),
+      helpText: 'Select a date',
+    );
+    if (newDate != null) {
+      setState(() {
+        _date = newDate;
+      });
+    }
+  }
+
+  DateTime _date = DateTime(2020, 11, 17);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: null,
+      width: 200,
+      height: 200,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _selectDate,
+              child: Text('SELECT DATE'),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Selected date: $_date',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
