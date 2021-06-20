1ed2f194e9f93d5721f34bdd3d8732ef09502496
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-06-20 12:55:02 +0200

    Added basic test

--- backend/src/test/virtualentity.test.ts
+++ backend/src/test/virtualentity.test.ts
@@ -1,11 +1,63 @@
 import { createConnection } from "typeorm";
 
 import {app} from '../index';
+import request from 'supertest';
 
+beforeAll(async () => {
+    await createConnection({
+        type: 'postgres',
+        host: 'db',
+        port: 5432,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: 'test',
+        synchronize: true,
+        logging: false,
+        entities: ['src/database/entity/**/*.ts'],
+        migrations: ['src/database/migration/**/*.ts'],
+        subscribers: ['src/database/subscriber/**/*.ts'],
+        cli: {
+            entitiesDir: 'src/database/entity',
+            migrationsDir: 'src/database/migration',
+            subscribersDir: 'src/database/subscriber'
+        }
+    });
+})
 
 describe('Testing if tests work', () => {
-    it('Should pass', () => {
-        let i = 2 + 10;
-        expect(i).toBe(12);
+    test('Should pass', async () => {
+        const response = await request(app).post('/virtualEntity/createVirtualEntity').send({
+            lesson_id: 1,
+            title: "Virtual Entity",
+            description: "The first actual virtual entity",
+            quiz: {
+                title: "The quiz",
+                description: "Info about the quiz",
+                questions: [
+                    {
+                        type: "TrueFalse",
+                        question: "What is the answer",
+                        options: ["True", "False"],
+                        correctAnswer: "True"
+                    },
+                    {
+                        type: "MultipleChoice",
+                        question: "What is the second answer",
+                        options: ["A", "B", "C", "D"],
+                        correctAnswer: "B"
+                    }
+                ]
+            },
+            model: {
+                name: "A New Model",
+                description: "Info about the model",
+                file_link: "http://model",
+                file_size: 36.2,
+                file_type: "obj",
+                file_name: "model_1",
+                preview_img: ""
+            }
+        })
+        expect(response.statusCode).toBe(200);
     })
 })
---
+++ backend/src/virtualEntity/validate.ts
@@ -0,0 +1,8 @@
+export const validateCreateVirtualEntityRequest = (body: any) => {
+    let keys = ["lesson_id", "title", "description"];
+    let model_keys = ["name", "file_link", "file_size", "file_name", "file_type"];
+    let quiz_keys = ["title", "description"];
+    let question_keys = ["type", "question"];
+
+
+}
