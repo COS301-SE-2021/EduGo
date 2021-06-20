be934f10020f11b982142ea278b2f10ce2fe102c
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-06-20 12:11:30 +0200

    feat: link form content

--- edugo_web_app/lib/ui/Views/SignIn/SignInPageContent.dart
+++ edugo_web_app/lib/ui/Views/SignIn/SignInPageContent.dart
@@ -1,1 +1,18 @@
+import 'package:edugo_web_app/ui/Views/SignIn/SignInPageForm.dart';
 import 'package:flutter/material.dart';
+
+class SignInPageContent extends StatelessWidget {
+  const SignInPageContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: 400,
+        child: Card(
+          child: SignInPageForm(),
+        ),
+      ),
+    );
+  }
+}
