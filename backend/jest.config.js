2d9900c2cbdab1c7cd3f95535552b943ff8de8ff
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-21 05:50:39 +0200

    fixed jest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,23 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-  "transform": {
-    "^.+\\.(ts|js|html)$": "ts-jest"
-}
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-  "setupFiles": [
-    "<rootDir>/test/setupTests.ts"
-  ]
+	preset: "ts-jest",
+	testEnvironment: "node",
 };
-=======
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
->>>>>>> parent of 71e8483 (Update jest.config.js)
