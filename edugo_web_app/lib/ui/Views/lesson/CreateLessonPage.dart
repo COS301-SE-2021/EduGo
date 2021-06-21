609b9ef781f1336cc86278e44f5e0f858a3ce283
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-06-21 03:53:03 +0200

    feat: update Lesson files

--- edugo_web_app/lib/ui/Views/lesson/CreateLessonPage.dart
+++ edugo_web_app/lib/ui/Views/lesson/CreateLessonPage.dart
@@ -1,9 +1,149 @@
+import 'package:edugo_web_app/ui/Views/lesson/CreateDate.dart';
+import 'package:edugo_web_app/ui/Views/lesson/CreateEndTime.dart';
+import 'package:edugo_web_app/ui/Views/lesson/CreateStartTime.dart';
+import 'package:edugo_web_app/ui/Views/lesson/LessonPage.dart';
+import 'package:edugo_web_app/ui/Views/virtual_entity/VirtualEntityPage.dart';
+import 'package:edugo_web_app/ui/Views/virtual_entity/VirtualEntityStorePage.dart';
+import 'package:edugo_web_app/ui/widgets/EduGoContainer.dart';
 import 'package:edugo_web_app/ui/widgets/EduGoPage.dart';
+import 'package:edugo_web_app/ui/widgets/input_fields/EduGoInput.dart';
+import 'package:edugo_web_app/ui/widgets/input_fields/EduGoMultiLineInput.dart';
 import 'package:flutter/material.dart';
 
-class CreateLessonPage extends StatelessWidget {
+class CreateLesonPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return EduGoPage(child: Text("Create Lesson Page"));
+    return EduGoPage(
+      child: Stack(
+        children: <Widget>[
+          EduGoContainer(
+            width: MediaQuery.of(context).size.width - 100,
+            height: MediaQuery.of(context).size.height - 100,
+            child: Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text("Create Lesson", style: TextStyle(fontSize: 25)),
+                ),
+                SizedBox(height: 5),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height - 360,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: <Widget>[
+                              SizedBox(height: 25),
+                              EduGoInput(
+                                  hintText: "Enter the lesson name",
+                                  width: 450),
+                              SizedBox(height: 25),
+                              EduGoMultiLineInput(
+                                  hintText: "Enter the lesson description",
+                                  maxLines: 4,
+                                  width: 450),
+                              SizedBox(height: 25),
+                              // Create entity
+                              MaterialButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10))),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            VirtualEntityPage()),
+                                  );
+                                },
+                                minWidth: 400,
+                                height: 60,
+                                child: Text("Create Virtual Entity",
+                                    style: TextStyle(color: Colors.white)),
+                                color: Color.fromARGB(255, 97, 211, 87),
+                              ),
+                              //
+                              SizedBox(height: 25),
+                              // Upload entity
+                              MaterialButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10))),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            VirtualEntityStore()),
+                                  );
+                                },
+                                minWidth: 400,
+                                height: 60,
+                                child: Text("Upload Virtual Entity",
+                                    style: TextStyle(color: Colors.white)),
+                                color: Color.fromARGB(255, 97, 211, 87),
+                              ),
+                              //
+                              SizedBox(height: 15),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 100),
+                      Container(
+                        height: MediaQuery.of(context).size.height - 350,
+                        width: 500,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(40),
+                            topLeft: Radius.circular(40),
+                            bottomLeft: Radius.circular(40),
+                            bottomRight: Radius.circular(40),
+                          ),
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 1,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Column(
+                              children: <Widget>[
+                                CreateDate(),
+                                CreateStartTime(),
+                                CreateEndTime(),
+                                MaterialButton(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(10))),
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => LessonPage()),
+                                    );
+                                  },
+                                  minWidth:
+                                      MediaQuery.of(context).size.width / 5,
+                                  height: 60,
+                                  child: Text("Add Lesson",
+                                      style: TextStyle(color: Colors.white)),
+                                  color: Color.fromARGB(255, 97, 211, 87),
+                                ),
+                              ],
+                            )),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
