ca2082faf86213aa7923e59f3e93090d212fc66b
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-06-26 23:31:11 +0200

    Consolidated model upload endpoints

--- backend/src/virtualEntity/api/virtualEntityController.ts
+++ backend/src/virtualEntity/api/virtualEntityController.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import {CreateVirtualEntityRequest} from '../model/CreateVirtualEntityRequest';
-import { GetVirtualEntitiesRequest } from '../model/GetVirtualEntitiesRequest';
 import { GetVirtualEntityRequest } from '../model/GetVirtualEntityRequest';
 import { VirtualEntityService } from '../service/virtualEntityService';
 import { VirtualEntityServiceImplementation } from '../service/virtualEntityServiceImplentation';
-import {router as modelController} from './modelController';
 import { validateCreateVirtualEntityRequest } from '../validate';
+import { uploadFile } from '../../fileUpload';
+import { AddModelToVirtualEntityFileData, AddModelToVirtualEntityRequest } from '../model/AddModelToVirtualEntityRequest';
+import { AddModelToVirtualEntityResponse } from '../model/AddModelToVirtualEntityResponse';
+
 const router = express.Router();
 const service: VirtualEntityService = new VirtualEntityServiceImplementation();
-router.use('/model', modelController);
 
 router.post('/createVirtualEntity', async (req, res) => {
     let valid = validateCreateVirtualEntityRequest(req.body);
@@ -28,6 +29,52 @@ router.post('/createVirtualEntity', async (req, res) => {
         res.json(valid);
     }
     
+})
+
+router.post('/uploadModel', uploadFile.single('file'), async (req, res) => {
+    const file: Express.MulterS3.File = <Express.MulterS3.File>req.file;
+
+    if (file == undefined)
+        res.status(400).json({message: 'Please upload a file'});
+
+    let response: any = {
+        file_name: file.key,
+        file_size: file.size,
+        file_type: file.key.split('.')[file.key.split('.').length-1],
+        file_link: file.location
+    }
+    res.status(200).json(response);
+})
+
+router.post('addToVirtualEntity', uploadFile.single('file'), async (req, res) => {
+    const file: Express.MulterS3.File = <Express.MulterS3.File>req.file;
+    let body: AddModelToVirtualEntityRequest = <AddModelToVirtualEntityRequest>req.body;
+
+    if (file == undefined)
+        res.status(400).json({message: 'Please upload a file'});
+    
+        let baseFile = {
+            file_name: file.key,
+            file_link: file.location,
+            file_type: file.key.split('.')[file.key.split('.').length-1],
+            file_size: file.size
+        }
+    
+        let data: AddModelToVirtualEntityFileData = {
+            id: body.virtualEntity_id,
+            description: body.description,
+            name: body.name,
+            ...baseFile
+        }
+
+        service.AddModelToVirtualEntity(data).then(response => {
+            let resp: AddModelToVirtualEntityResponse = {
+                model_id: response.model_id,
+                ...body,
+                ...baseFile
+            }
+            res.status(200).json(resp)
+        })
 })
 
 router.post('/getVirtualEntities', async (req, res) => {
