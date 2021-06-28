07b3f0b49460fa7900ffcf8f9e6232f0bc680c06
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-28 23:02:01 +0200

    passport config done

--- backend/src/user/api/userController.ts
+++ backend/src/user/api/userController.ts
@@ -1,5 +1,7 @@
 import express from "express";
-
+import { register } from "../service/userService";
+import { RegisterRequest } from "../models/registerRequest";
+import { json } from "body-parser";
 const router = express.Router();
 
 router.use((req, res, next) => {
@@ -10,6 +12,13 @@ router.get("/protected", async (req, res, next) => {});
 
 router.post("/login", async (req, res) => {});
 
-router.post("/register", async (req, res) => {});
+router.post("/register", async (req, res) => {
+	register(<RegisterRequest>req.body).then((result) => {
+		if (result?.type == "success") {
+			res.json(result);
+			res.status(200);
+		}
+	});
+});
 
 export { router };
