0bb7b9e483e2ea30ae234c7c5c3f4beeb0ab961a
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-06-29 21:48:36 +0200

    Jwt token working

--- backend/src/auth/service/authService.ts
+++ backend/src/auth/service/authService.ts
@@ -1,11 +1,13 @@
 import { ApiResponse } from "../../models/apiResponse";
 import { Lesson } from "../../database/entity/Lesson";
-import { Any, createConnection, getConnection } from "typeorm";
+import { Any, createConnection, getConnection, getRepository } from "typeorm";
 import { Subject } from "../../database/entity/Subject";
 import { Educator } from "../../database/entity/Educator";
 import { Student } from "../../database/entity/Student";
+import { User } from "../../database/entity/User";
 import utils from "../lib/utils";
 import { RegisterRequest } from "../models/RegisterRequest";
+import { LoginRequest } from "../models/LoginRequest";
 
 let statusRes: any = {
 	message: "",
@@ -16,56 +18,64 @@ let statusRes: any = {
 export async function register(request: RegisterRequest) {
 	let conn = getConnection();
 	// registration for a student
-	if (request.userType.toLowerCase() == "educator") {
-		let user = new Educator();
-		user.email = request.email;
-		user.firstName = request.firstName;
-		user.lastName = request.lastName;
-		user.username = request.username;
-		user.organizationId = request.organizationId;
-		user.verified = false;
-		const saltHAsh = utils.genPassword(request.password);
-		user.salt = saltHAsh.salt;
-		user.hash = saltHAsh.hash;
+	let user = new User();
+	user.email = request.email;
+	user.firstName = request.firstName;
+	user.lastName = request.lastName;
+	user.username = request.username;
+	user.organizationId = request.organizationId;
+	user.verified = false;
+	const saltHAsh = utils.genPassword(request.password);
+	user.salt = saltHAsh.salt;
+	user.hash = saltHAsh.hash;
+	user.isAdmin = false;
+	user.type = request.userType;
+	//user.admin = false;
+	let userRepo = conn.getRepository(User);
 
-		user.admin = false;
-		let userRepo = conn.getRepository(Educator);
+	return userRepo
+		.save(user)
+		.then((result) => {
+			statusRes.message = "Educator registered";
+			statusRes.type = "success";
+			return statusRes;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
 
-		return userRepo.save(user).then((result) => {
-			let isValid = utils.validPassword(
+export async function login(request: LoginRequest) {
+	let UserRepo = getRepository(User);
+
+	// Find user with given Username
+	return UserRepo.findOne({ where: { username: request.username } })
+		.then((user) => {
+			if (!user) {
+				statusRes.message = "User doesn't exist";
+				return statusRes;
+			}
+
+			// validate password of user
+			let isvalid = utils.validPassword(
 				request.password,
 				user.hash,
 				user.salt
 			);
-			if (isValid) {
-				statusRes.token = utils.issueJWT(user);
-				statusRes.message = "Educator registered";
+
+			if (isvalid) {
+				// issue jwt token for the user
+
+				statusRes.token = utils.issueJWT(user.id);
+				statusRes.message = "User Logged in";
 				statusRes.type = "success";
+				return statusRes;
+			} else {
+				statusRes.message = "Password is invalid";
+				return statusRes;
 			}
-
-			return statusRes;
+		})
+		.catch((err) => {
+			console.log(err);
 		});
-	} else {
-		// registration for an educator
-		let user = new Student();
-		user.email = request.email;
-		user.firstName = request.firstName;
-		user.lastName = request.lastName;
-		user.username = request.username;
-		user.organizationId = request.organizationId;
-		user.verified = false;
-		const saltHAsh = utils.genPassword(request.password);
-		user.salt = saltHAsh.salt;
-		user.hash = saltHAsh.hash;
-
-		let userRepo = conn.getRepository(Student);
-
-		return userRepo.save(user).then((result) => {
-			statusRes.message = "Student registered";
-			statusRes.type = "success";
-			return statusRes;
-		});
-	}
 }
-
-export async function login() {}
