bb84841839e7b41e97cf63341fb1c55ea91f2ffd
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-07-01 15:54:46 +0200

    get all three categories of users

--- backend/src/user/validate.ts
+++ backend/src/user/validate.ts
@@ -3,3 +3,7 @@ const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 export const validateEmail = (email: string): boolean => {
     return emailRegex.test(email)
 }
+
+export const validateEmails = (emails: string[]): boolean => {
+    return emails.every(value => validateEmail(value));
+}
