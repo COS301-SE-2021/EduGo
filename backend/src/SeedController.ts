4533327c45d8d42b826384181b16bec45d9e2e06
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-07-05 17:33:47 +0200

    Added seeding functionality

---
+++ backend/src/SeedController.ts
@@ -0,0 +1,73 @@
+import seed from './seed.json';
+import express from 'express'
+import { getRepository } from 'typeorm';
+import { Organisation } from './database/entity/Organisation';
+import { User } from './database/entity/User';
+import { Student } from './database/entity/Student';
+import { Educator } from './database/entity/Educator';
+
+const router = express.Router();
+
+router.post('/hidden/seed', async (req, res) => {
+    let organisationRepository = getRepository(Organisation);
+    let userRepository = getRepository(User);
+
+    let organisations: Organisation[] = seed.organisation.map(value => {
+        let organisation: Organisation = new Organisation();
+        organisation.id = value.id;
+        organisation.name = value.name;
+        organisation.email = value.email;
+        organisation.phone = value.phone;
+
+        return organisation;
+    })
+
+    let orgLength = organisations.length;
+
+    let users: User[] = seed.user.map((value, index) => {
+        let user: User = new User();
+        user.id = value.id;
+        user.username = value.username;
+        user.firstName = value.firstName;
+        user.lastName = value.lastName;
+        user.email = value.email;
+        user.hash = '';
+        user.salt = '';
+        if (index % 2 === 0) {
+            //Is student
+            user.student = new Student();
+        }
+        else {
+            //Is educator
+            user.educator = new Educator();
+            user.educator.admin = false;
+        }
+        user.organisation = organisations[Math.floor(Math.random() * orgLength)]
+        return user;
+    })
+
+    organisationRepository.save(organisations).then(savedOrgs => {
+        userRepository.save(users).then(savedUsers => {
+            let response = {
+                org_count: savedOrgs.length,
+                user_count: savedUsers.length
+            }
+            res.status(200).json(response)
+        })
+    })
+})
+
+router.get('/hidden/seed', async (req, res) => {
+    let organisationRepository = getRepository(Organisation);
+    let userRepository = getRepository(User);
+
+    // userRepository.findByIds([1,2,3,4,5,6,7,8], {relations: ['organisation']}).then(users => {
+    //     res.status(200).json(users);
+    // })
+
+    organisationRepository.findByIds([1, 2], {relations: ['users', 'users.student', 'users.educator']}).then(orgs => {
+        res.status(200).json(orgs);
+    })
+})
+
+export { router };
