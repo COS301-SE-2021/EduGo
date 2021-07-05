251d8c12064b327800749d9c9996e41720af7450
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-07-05 21:12:29 +0200

    Add educator endpoint

--- backend/src/user/api/userController.ts
+++ backend/src/user/api/userController.ts
@@ -4,21 +4,35 @@ import express from "express";
 import { json } from "body-parser";
 import { AddStudentsToSubjectRequest } from "../models/AddStudentsToSubjectRequest";
 import { StudentService } from "../service/StudentServiceImplementation";
+import { EducatorService } from "../service/EducatorServiceImplementation";
+import { AddEducatorsRequest } from "../models/AddEducatorsRequest";
 const router = express.Router();
 
 router.use((req, res, next) => {
 	next();
 });
 
-const service: StudentService = new StudentService();
+const studentService: StudentService = new StudentService();
+const educatorService: EducatorService = new EducatorService();
 
-router.post('/addUsersToSubject', async (req, res) => {
+router.post('/addStudentsToSubject', async (req, res) => {
 	let body: AddStudentsToSubjectRequest = <AddStudentsToSubjectRequest>req.body;
-	service.AddUsersToSubject(body).then(() => {
+	studentService.AddUsersToSubject(body).then(() => {
 		res.status(200).send('ok')
 	})
 	.catch(err => {
 		res.status(400).json(err);
+	})
+})
+
+router.post('/addEducators', async (req, res) => {
+	let body: AddEducatorsRequest = <AddEducatorsRequest>req.body;
+
+	educatorService.AddEducators(body).then(() => {
+		res.status(200).send('ok')
+	})
+	.catch(err => {
+		res.status(400).json(err)
 	})
 })
 
