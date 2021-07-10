4ab15bffb9bd9ec26b2ccd0205fcc26617f7be0c
Mihlali <u19187166@tuks.co.za>
2021-07-10 16:09:05 +0200

    feat: add settings title

--- mobile/lib/settings_page/settings.dart
+++ mobile/lib/settings_page/settings.dart
@@ -1,1 +1,34 @@
 import 'package:flutter/material.dart';
+class Settings extends StatefulWidget {
+  Settings({Key key}) : super(key: key);
+  @override
+  _SettingsState createState() => new _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+    @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('App Name'),
+          ),
+        body:
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new Text(
+              "Settings",
+                style: new TextStyle(fontSize:34.0,
+                color: const Color(0xFF000000),
+                fontWeight: FontWeight.w500,
+                fontFamily: "Roboto"),
+              )
+            ]
+    
+          ),
+    
+      );
+    }
+}
