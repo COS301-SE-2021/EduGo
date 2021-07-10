1c27eb85359f7161e2fa755fb47e179ef1ff042b
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-07-10 21:37:01 +0200

    feat: add imported controller

---
+++ mobile/lib/common_widgets/nav_containers/bottom_nav_bar.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+
+PersistentTabController _controller;
+_controller = PersistentTabController(initialIndex: 0);
+
