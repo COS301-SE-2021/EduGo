b4b608d90f219fd93cd75d78dac14d6a0103e339
Mihlali <u19187166@tuks.co.za>
2021-07-10 16:26:59 +0200

    feat: wrap in fractionally sized box

--- mobile/lib/settings_page/settings.dart
+++ mobile/lib/settings_page/settings.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+
 class Settings extends StatefulWidget {
-  Settings({Key key}) : super(key: key);
   @override
   _SettingsState createState() => new _SettingsState();
 }
@@ -9,24 +9,26 @@ class _SettingsState extends State<Settings> {
     @override
     Widget build(BuildContext context) {
       return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('App Name'),
-          ),
         body:
-          new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              new Text(
-              "Settings",
-                style: new TextStyle(fontSize:34.0,
-                color: const Color(0xFF000000),
-                fontWeight: FontWeight.w500,
-                fontFamily: "Roboto"),
-              )
-            ]
+          new FractionallySizedBox(
+            child:
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(
+                  "Settings",
+                    style: new TextStyle(fontSize:48.0,
+                    color: const Color(0xFF000000),
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "Roboto"),
+                  )
+                ]
     
+              ),
+    
+            widthFactor: 1.00, heightFactor: 1.00,
           ),
     
       );
