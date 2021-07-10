2ea8f604c7a9d7b0f7ff7cc01a74f1fa0b29a947
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-07-10 21:53:40 +0200

    feat: update dependancies and create widget options

--- mobile/lib/common_widgets/nav_containers/bottom_nav_bar.dart
+++ mobile/lib/common_widgets/nav_containers/bottom_nav_bar.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/pages/DetectMarkerPage.dart';
+import 'package:mobile/pages/GradesPage.dart';
+import 'package:mobile/pages/HomePage.dart';
+import 'package:mobile/pages/SubjectsPage.dart';
 
-class Bottom_Nav_Bar extends StatelessWidget {
-  const Bottom_Nav_Bar({Key? key}) : super(key: key);
+class Bottom_Nav_Bar extends StatefulWidget {
+  Bottom_Nav_Bar({Key? key}) : super(key: key);
+
+  @override
+  _Bottom_Nav_BarState createState() => _Bottom_Nav_BarState();
+}
+
+class _Bottom_Nav_BarState extends State<Bottom_Nav_Bar> {
+  //widgets (screens) based on icon selected in bottom nav bar
+  int _selectedIndex = 0;
+  // (Home, Subject, Grade, DetectMarker)
+  List<Widget> _widgetOptions = <Widget>[
+    HomePage(),
+    SubjectsPage(),
+    GradesPage(),
+    DetectMarkerPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
---
+++ mobile/lib/pages/SubjectsPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SubjectsPage extends StatelessWidget {
+  const SubjectsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
