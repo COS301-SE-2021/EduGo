51995fa3b54d0aef3c38b528ccd1e6dc1bb91fd7
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-07-11 15:19:10 +0200

    feat: create MVC folder structure, routing in side nav, User mode

---
+++ mobile/lib/src/Pages/PreferencesPage/View/PreferencesPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PreferencesPage extends StatefulWidget {
+  PreferencesPage({Key? key}) : super(key: key);
+
+  @override
+  _PreferencesPageState createState() => _PreferencesPageState();
+}
+
+class _PreferencesPageState extends State<PreferencesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Preferences"),
+    );
+  }
+}
+//D:\Users\u19187166\Desktop\EduGo\mobile\lib\src\assets
