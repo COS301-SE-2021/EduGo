319bc56bcbe823baa5ce6b00b00dddba97eff8ac
kieran-woodrow <77970893+kieran-woodrow@users.noreply.github.com>
2021-07-11 10:06:01 +0200

    started login page for mobile app

---
+++ mobile/lib/src/Pages/Login Page/View/LoginPage.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:momentum/momentum.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
