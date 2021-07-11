c361d2befc11c6d97f1bc33a56df0c905948f15f
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-07-11 17:07:56 +0200

    feat: create pages for profile & preferences

---
+++ mobile/lib/src/Pages/PreferencesPage/Controller/Preferences.dart
@@ -0,0 +1,10 @@
+import 'package:mobile/src/Pages/PreferencesPage/Model/User.dart';
+
+class Preferences {
+  static const user = User(
+      image_path:
+          "D:\Users\u19187166\Desktop\EduGo\mobile\lib\src\assets\profile.jpg",
+      name: "Mihlali Mgwebi",
+      isDarkMode: false,
+      current_organistion: "COS Crash Course");
+}
