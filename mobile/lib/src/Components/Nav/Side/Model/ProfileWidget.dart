a78bbd528c138a7504d0c6045da63fa2df5120d4
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-07-19 04:56:10 +0200

    fix: add image

--- mobile/lib/src/Components/Nav/Side/Model/ProfileWidget.dart
+++ mobile/lib/src/Components/Nav/Side/Model/ProfileWidget.dart
@@ -11,60 +11,18 @@ class ProfileWidget extends StatelessWidget {
     this.isEdit = false,
     required this.onClicked,
   }) : super(key: key);
-
+//todo when click on image can select an image to upload
   @override
   Widget build(BuildContext context) {
-    final color = Theme.of(context).colorScheme.primary;
     return Center(
-      child: Stack(
-        children: [
-          //buildImage(),
-          ClipOval(
-            child: Material(
-              color: Colors.transparent,
-              child: Ink.image(
-                image: AssetImage(image_path),
-                fit: BoxFit.cover,
-                width: 128,
-                height: 128,
-                child: InkWell(onTap: onClicked),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            right: 4,
-            child: buildEditIcon(color),
-          ),
-        ],
-      ),
-    );
+        child: Stack(children: [
+      CircleAvatar(
+        radius: 30.0,
+        backgroundImage: AssetImage(
+          "../assets/images/profile.jpg",
+        ),
+        backgroundColor: Colors.transparent,
+      )
+    ]));
   }
-
-  Widget buildEditIcon(Color color) => buildCircle(
-        color: Colors.white,
-        all: 3,
-        child: buildCircle(
-          color: color,
-          all: 8,
-          child: Icon(
-            isEdit ? Icons.add_a_photo : Icons.edit,
-            color: Colors.white,
-            size: 20,
-          ),
-        ),
-      );
-
-  Widget buildCircle({
-    required Widget child,
-    required double all,
-    required Color color,
-  }) =>
-      ClipOval(
-        child: Container(
-          padding: EdgeInsets.all(all),
-          color: color,
-          child: child,
-        ),
-      );
 }
--- mobile/lib/src/Pages/PreferencesPage/Controller/Preferences.dart
+++ mobile/lib/src/Pages/PreferencesPage/Controller/Preferences.dart
@@ -2,8 +2,7 @@ import 'package:mobile/src/Pages/PreferencesPage/Model/User.dart';
 
 class Preferences {
   static const user = User(
-      image_path:
-          "D:\Users\u19187166\Desktop\EduGo\mobile\lib\src\assets\profile.jpg",
+      image_path: "assets/images/profile.jpg",
       name: "Mihlali Mgwebi",
       isDarkMode: false,
       current_organistion: "COS Crash Course");
