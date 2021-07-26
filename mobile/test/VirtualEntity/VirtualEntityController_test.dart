8b68bc602394b65dba902a5f2969a0de11127233
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-07-26 11:45:52 +0200

    TODO fix virtual entity controller mocks in tests

--- mobile/test/VirtualEntity/VirtualEntityController_test.dart
+++ mobile/test/VirtualEntity/VirtualEntityController_test.dart
@@ -1,11 +1,53 @@
 import 'package:http/http.dart' as http;
+import 'package:mobile/src/Pages/VirtualEntityPage/Models/VirtualEntityModels.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mobile/src/Pages/VirtualEntityPage/Controller/VirtualEntityController.dart';
+import 'package:mobile/globals.dart';
+import 'VirtualEntityController_test.mocks.dart';
 
 @GenerateMocks([http.Client])
 void main() {
+  group('Virtual Entity', () {
+    test('should fetch virtual entity if the http call completes successfully', () async {
+      final client = MockClient();
+      when(client.post(Uri.parse("${baseUrl}virtualEntity/getVirtualEntity"))).thenAnswer((_) async => http.Response('''
+        {
+          "id": "1",
+          "title": "Virtual Entity",
+          "description": "Virtual Entity Description",
+          "model": {
+            "id": "1",
+            "name": "Virtual Entity Model",
+            "description": "Virtual Entity Model Description",
+            "file_name": "Virtual Entity Model File Name",
+            "file_type": "Virtual Entity Model File Type",
+            "file_size": 0,
+            "file_link": "Virtual Entity Model File Link"
+          },
+          "quiz": {
+            "id": "1",
+            "name": "Virtual Entity Quiz",
+            "description": "Virtual Entity Quiz Description",
+            "questions": [
+              {
+                "id": "1",
+                "question": "Virtual Entity Question",
+                "type": "TrueFalse"
+                "correctAnswer": "True",
+                "options": [
+                  "True",
+                  "False"
+                ]
+              }
+            ]
+          }
+        }
+      ''', 200));
 
+      expect(await getVirtualEntity(1, client: client), isA<VirtualEntity>());
+    });
+  });
 }
