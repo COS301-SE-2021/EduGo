09d251a598bfc6248729740bfc23edfd59b3f07d
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-07-27 23:01:02 +0200

    bug: display issue

--- mobile/lib/src/Pages/HomePage/View/HomePage.dart
+++ mobile/lib/src/Pages/HomePage/View/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/src/Components/mobile_page_layout.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -10,8 +11,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Home"),
+    return MobilePageLayout(
+      Text("Home"),
     );
   }
 }
