15193017f61c8a323ad187cf3f180a6d8515b013
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-07-28 12:46:28 +0200

    feat: accept changes

--- mobile/lib/src/Pages/PreferencesPage/View/PreferencesPage.dart
+++ mobile/lib/src/Pages/PreferencesPage/View/PreferencesPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/src/Pages/PreferencesPage/Controller/Preferences.dart';
 
 class PreferencesPage extends StatefulWidget {
   PreferencesPage({Key? key}) : super(key: key);
@@ -8,11 +7,7 @@ class PreferencesPage extends StatefulWidget {
   _PreferencesPageState createState() => _PreferencesPageState();
 }
 
-//todo use controller and momentum to manipulate user detils
 class _PreferencesPageState extends State<PreferencesPage> {
-  //user varaibles used to access
-  final user = Preferences.user;
-
   @override
   Widget build(BuildContext context) {
     return Container(
