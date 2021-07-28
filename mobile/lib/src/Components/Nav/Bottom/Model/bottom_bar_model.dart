cfc7e4bd38e916528553a2316354a0b1cfd3464a
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-07-28 11:08:34 +0200

    bug: bottom bar routing not working
    solution: display based on bool values

---
+++ mobile/lib/src/Components/Nav/Bottom/Model/bottom_bar_model.dart
@@ -0,0 +1,27 @@
+//TODO SISI, DO NOT USE Momentum, just take your model chosen and display that
+//Oh my days, I was wrong: Use momentum, set child widget as default for
+//So I do need the chld uh ha ha
+
+//This file stores the data: the screens displayed with their relevant content
+import 'package:flutter/material.dart';
+
+//Imported, custom pages
+import 'package:mobile/src/Pages/DetectMarkerPage/View/DetectMarkerPage.dart';
+import 'package:mobile/src/Pages/GradesPage/View/GradesPage.dart';
+import 'package:mobile/src/Pages/HomePage/View/HomePage.dart';
+import 'package:mobile/src/Pages/SubjectsPage/View/SubjectsPage.dart';
+
+//constructor: default page is displayed. Once a tab is selected, selected page will be displayed
+class BottomBarModel {
+  const BottomBarModel(this.currentPageDisplayed);
+  final Widget currentPageDisplayed;
+}
+
+//all the different pages that will display based on the tab selected
+//this list allows for agile development as more icons can be added anytime.
+List<Widget> widgetOptions = <Widget>[
+  HomePage(),
+  SubjectsPage(),
+  GradesPage(),
+  DetectMarkerPage(),
+];
--- mobile/lib/src/Components/mobile_page_layout.dart
+++ mobile/lib/src/Components/mobile_page_layout.dart
@@ -4,22 +4,35 @@ import 'package:mobile/src/Components/Nav/Bottom/View/bottom_bar.dart';
 import 'package:mobile/src/Components/Nav/Side/View/SideBar.dart';
 
 class MobilePageLayout extends StatelessWidget {
+// wrap
   const MobilePageLayout(
     this.child,
+    this.isSideBarVisible,
+    this.isBottomBarVisible,
   );
   final Widget child;
+  final bool isSideBarVisible;
+  final bool isBottomBarVisible;
 
   @override
   Widget build(BuildContext context) {
+    final BottomBar bottomBar = BottomBar(this);
     return Scaffold(
-      drawer: SideBar(),
+      drawer: Visibility(child: SideBar(), visible: isSideBarVisible),
       appBar: AppBar(
         title: Text('EduGo'),
         backgroundColor: Color.fromARGB(255, 97, 211, 87),
       ),
       backgroundColor: Colors.white,
-      bottomNavigationBar: BottomBar(),
-      body: child ?? widgetOptions.elementAt(selectedIndex),
+      bottomNavigationBar:
+          Visibility(child: bottomBar, visible: isBottomBarVisible),
+      body:
+          child, //alrights, sometimes, I want the screen only (visibles, sorted), but my bottom bar is not working. WHy
+      //because when i click, the body doesn;t update
+      //so how dp  get the child to update the parren't body
+      //- callbacks?
+      //- parameters
+      //momentum itself?
     );
   }
 }
