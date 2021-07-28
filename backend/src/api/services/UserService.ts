e836cb67abd05204b297e308796b68c883162cdf
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-07-28 13:16:03 +0200

    SetToAdmin and Revoke from admin added

--- backend/src/api/services/UserService.ts
+++ backend/src/api/services/UserService.ts
@@ -3,17 +3,57 @@ import { getRepository } from "typeorm";
 import { RevokeUserFromAdminRequest } from "../models/user/RevokeUserFromAdminRequest";
 import { SetUserToAdminRequest } from "../models/user/SetUserToAdminRequet";
 import { DatabaseError } from "../errors/DatabaseError";
+import { handleSavetoDBErrors } from "../helper/ErrorCatch";
+import { NonExistantItemError } from "../errors/NonExistantItemError";
+import { InvalidParameterError } from "../errors/InvalidParametersError";
 
 export class UserService {
 	public async setUserToAdmin(request: SetUserToAdminRequest) {
 		if (request.username == null) {
-			//TO DO EXCEPTION
-			return null;
+			throw new InvalidParameterError("Username not provided");
 		}
 		// TO DO change this to cater for Educators not user
 		let userRepo = getRepository(User);
 		let username = request.username;
-		let user = await userRepo.findOne({ where: { username: username } });
+		let user = await userRepo.findOne({
+			where: { username: username },
+			relations: ["educator"],
+		});
+
+		if (user) {
+			if (user.educator) {
+				if (!user.educator.admin) {
+					user.educator.admin = true;
+					userRepo
+						.save(user)
+						.then((saved) => {
+							return true;
+						})
+						.catch((err) => {
+							handleSavetoDBErrors(err);
+						});
+				} else {
+					throw new DatabaseError("User is already admin");
+				}
+			} else {
+				throw new NonExistantItemError("User is not an educator");
+			}
+		} else {
+			throw new NonExistantItemError("user not found");
+		}
+	}
+
+	public async revokeUserFromAdmin(request: RevokeUserFromAdminRequest) {
+		if (request.username == null) {
+			throw new InvalidParameterError("Username not provided");
+		}
+
+		let userRepo = getRepository(User);
+		let username = request.username;
+		let user = await userRepo.findOne({
+			where: { username: username },
+			relations: ["educator"],
+		});
 
 		if (user) {
 			if (user.educator) {
@@ -25,56 +65,16 @@ export class UserService {
 							return true;
 						})
 						.catch((err) => {
-							throw DatabaseError
-							return false;
+							handleSavetoDBErrors(err);
 						});
 				} else {
-					// user is already admin
+					throw new DatabaseError("User is already not an admin");
 				}
 			} else {
-				// add user is not an educator
-				return false;
+				throw new NonExistantItemError("User is not an educator");
 			}
 		} else {
-			// add user not found exception
-			return false;
-		}
-	}
-
-	public async revokeUserFromAdmin(request: RevokeUserFromAdminRequest) {
-		if (request.username == null) {
-			//TODO EXCEPTION
-			return false;
-		}
-		
-		let userRepo = getRepository(User);
-		let username = request.username;
-		let user = await userRepo.findOne({ where: { username: username } });
-
-		if (user) {
-			if (user.educator) {
-				if (!user.educator.admin) {
-					user.educator.admin = false;
-					userRepo
-						.save(user)
-						.then((saved) => {
-							return true;
-						})
-						.catch((err) => {
-							// exception for database unable to save
-							console.log(err.message);
-							return false;
-						});
-				} else {
-					// user is already admin
-				}
-			} else {
-				// add user is not an educator
-				return false;
-			}
-		} else {
-			// add user not found exception
-			return false;
+			throw new NonExistantItemError("user not found");
 		}
 	}
 }
