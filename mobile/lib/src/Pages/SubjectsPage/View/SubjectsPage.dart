5be93ac6828c5f8c7b0dbea6bc0cb4482089db87
kieran-woodrow <77970893+kieran-woodrow@users.noreply.github.com>
2021-07-29 14:07:22 +0200

    integrating subjectcard widget into subject page

--- mobile/lib/src/Pages/SubjectsPage/View/SubjectsPage.dart
+++ mobile/lib/src/Pages/SubjectsPage/View/SubjectsPage.dart
@@ -20,8 +20,22 @@ class _SubjectsPageState extends State<SubjectsPage> {
         final subjects = snapshot<SubjectsModel>();
         //TODO replace the text widget with a List of subject card widgets, try using the map function to this in one line
         //Example: subjects.subjects.map(subject => SubjectCard(subject: subject));
-        return Text('');
+        return Container(
+          height: MediaQuery.of(context).size.height - 200,
+          padding: EdgeInsets.only(top: 75),
+          child: GridView.count(
+            childAspectRatio: MediaQuery.of(context).size.height / 1000,
+            primary: false,
+            padding: const EdgeInsets.all(20),
+            crossAxisSpacing: 30,
+            shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            mainAxisSpacing: 20,
+            crossAxisCount: 4,
+            children: //subjects.subjects.map(subject => SubjectCard(subject: subject));
+          ),
+        );
       },
-    ));
+    ),);
   }
 }
