18b75285f91b5337727c9d67b58d56d7ee702156
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-07-30 17:56:29 +0200

    added user login in the controller

--- mobile/lib/src/Components/User/Controller/UserController.dart
+++ mobile/lib/src/Components/User/Controller/UserController.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:http/testing.dart' as httpMock;
 import 'package:mobile/globals.dart';
+import 'package:mobile/src/Exceptions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-Future<void> login({required String username, required String password, required http.Client client}) async {
+Future<bool> login({required String username, required String password, required http.Client client}) async {
   final response = await client.post(
     Uri.parse("${baseUrl}auth/login"),
     headers: <String, String>{'Content-Type': 'application/json'},
@@ -17,7 +18,13 @@ Future<void> login({required String username, required String password, required
   if (response.statusCode == 200) {
     Map<String, dynamic> json = jsonDecode(response.body);
     if (json['token'] != null) {
-      
+      String token = json['token'] as String;
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString('user_token', token);
+      return true;
     }
+    else throw new BadResponse('No token property');
   }
+  else if (response.statusCode == 401) return false;
+  throw new Exception('Unexpected server error');
 }
