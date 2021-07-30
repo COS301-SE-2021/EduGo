a20963ab9d0f6af8b761ce8e3100eef328aa84e7
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-07-30 15:44:54 +0200

    fix: remove comments

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -37,17 +37,3 @@ class MyApp extends StatelessWidget {
         });
   }
 }
-/*
-MaterialApp(
-  title: 'Named Routes Demo',
-  // Start the app with the "/" named route. In this case, the app starts
-  // on the FirstScreen widget.
-  initialRoute: '/',
-  routes: {
-    // When navigating to the "/" route, build the FirstScreen widget.
-    '/': (context) => const FirstScreen(),
-    // When navigating to the "/second" route, build the SecondScreen widget.
-    '/second': (context) => const SecondScreen(),
-  },
-)
- */
