957a519cd060d2a753822835fa9c754cb0676c82
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-07-31 22:32:12 +0200

    Added user service to main

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/src/Components/User/Controller/UserController.dart';
+import 'package:mobile/src/Components/User/Service/UserService.dart';
 import 'package:mobile/src/Pages/DetectMarkerPage/View/DetectMarkerPage.dart';
 import 'package:mobile/src/Pages/GradesPage/View/GradesPage.dart';
 import 'package:mobile/src/Pages/HomePage/View/HomePage.dart';
@@ -24,6 +25,9 @@ Momentum momentum({bool mock = false}) {
       LessonsController(mock: mock), 
       SubjectsController(mock: mock),
       UserController(mock: mock),
+    ],
+    services: [
+      UserApiService(),
     ],
   );
 }
