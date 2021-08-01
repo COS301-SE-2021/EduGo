b4b00542816f287711f274b7e955182a106df2d1
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-08-01 22:26:37 +0200

    Started work on recommendation controller

---
+++ backend/src/api/controllers/RecommendationController.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import passport from "passport";
+import { isUser, RequestObjectWithUserId } from "../middleware/validate";
+
+const router = express.Router();
+
+// router.post(
+//     '/upcomingLessons', 
+//     passport.authenticate("jwt", { session: false }),
+//     isUser,
+//     (req: RequestObjectWithUserId, res) => {
+        
+//     }
+// );
