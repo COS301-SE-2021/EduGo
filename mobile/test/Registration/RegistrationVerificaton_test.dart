d0ccf5f3d4ddc4eecc79c023fb7de291c7bba883
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-08-05 20:32:25 +0200

    feat: add individual input tests

--- mobile/test/Registration/RegistrationVerificaton_test.dart
+++ mobile/test/Registration/RegistrationVerificaton_test.dart
@@ -7,19 +7,34 @@ import 'package:mobile/src/Pages/RegistrationPage/View/RegistrationVerificationP
 //utilize the WidgetTester utility for various things while testing, for example, sending input to a widget, finding a part in the widget tree, confirming values, etc.
 void main() {
   List<TextField> input_fields = [];
+  var email_input_field;
+  var code_input_field;
 
   // Define widget tests that test each widget's response to user interaction.
   testWidgets(
-      'Successfully find all input fields and add to list for future tests',
+      'Successfully find all input fields on page and add to list for future tests',
       (WidgetTester tester) async {
+    //find all text fields in on the page
     find.byType(TextField).evaluate().toList().forEach((element) {
       input_fields.add(element.widget as TextField);
     });
   });
 
+  testWidgets('Successfully find email input field',
+      (WidgetTester tester) async {
+    //find all email input field on the page
+    email_input_field = find.byKey(Key('email_input_field'));
+  });
+
+  testWidgets('Successfully find activation code input field',
+      (WidgetTester tester) async {
+    //find all activation code input field on the page
+    code_input_field = find.byKey(Key('code_input_field'));
+  });
+
   /*testWidgets('Successfully enter text into input field',
       (WidgetTester tester) async {
-    final email_input_field = find.byKey(Key('email_input_field'));
+    
     // Build the widget
     await tester.pumpWidget(RegistrationVerificationPage());
     //Tap TextFormField
@@ -33,9 +48,6 @@ void main() {
 
 /* 
 
-    find.byType(TextField).evaluate().toList().forEach((element) {
-      formFields.add(element.widget);
-    });
 
     formFields.forEach((element) {
       expect(element.maxLines, 1);
