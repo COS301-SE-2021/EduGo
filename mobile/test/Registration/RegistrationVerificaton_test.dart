ddd09de004edf5c6e4ccc6f83688d772a70f30ee
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-08-08 17:35:27 +0200

    feat: add test

--- mobile/test/Registration/RegistrationVerificaton_test.dart
+++ mobile/test/Registration/RegistrationVerificaton_test.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 import 'package:mobile/main.dart';
+import 'package:mobile/src/Components/User/Controller/UserController.dart';
 import 'package:mobile/src/Pages/RegistrationPage/View/RegistrationVerificationPage.dart';
+import 'package:momentum/momentum.dart';
 
 //Automated tests help guarantee that your application performs accurately before you publish it while holding your feature and bug-fix velocity
 //utilize the WidgetTester utility for various things while testing, for example, sending input to a widget, finding a part in the widget tree, confirming values, etc.
@@ -25,13 +27,19 @@ void main() {
 //testWidgets('successfully.', (WidgetTester tester) async {});
 ///////////////////////////// WIDGET TESTS /////////////////////////////////////
 void _widget_tests() {
-  //TODO implement test
   group('Text', () {
     testWidgets('form fields should render successfully.',
         (WidgetTester tester) async {
-      final widget = momentum();
+      final widget = Momentum(
+          child: MaterialApp(
+            home:
+                RegistrationVerificationPage(Key('registration_verification')),
+          ),
+          controllers: [
+            UserController(),
+          ]);
       await tester.pumpWidget(widget);
-      await tester.pumpAndSettle(const Duration(milliseconds: 100));
+      await tester.pumpAndSettle();
 
       // Find text form fields
       final textFieldFinder = find.byType(TextFormField);
@@ -40,9 +48,16 @@ void _widget_tests() {
     });
     testWidgets('form fields should be empty and display an error string.',
         (WidgetTester tester) async {
-      final widget = momentum();
+      final widget = Momentum(
+          child: MaterialApp(
+            home:
+                RegistrationVerificationPage(Key('registration_verification')),
+          ),
+          controllers: [
+            UserController(),
+          ]);
       await tester.pumpWidget(widget);
-      await tester.pumpAndSettle(const Duration(milliseconds: 100));
+      await tester.pumpAndSettle();
 
       // find empty text
       final textFinder = find.text('');
@@ -56,9 +71,16 @@ void _widget_tests() {
 
     testWidgets('headings for page should render successfully.',
         (WidgetTester tester) async {
-      final widget = momentum();
+      final widget = Momentum(
+          child: MaterialApp(
+            home:
+                RegistrationVerificationPage(Key('registration_verification')),
+          ),
+          controllers: [
+            UserController(),
+          ]);
       await tester.pumpWidget(widget);
-      await tester.pumpAndSettle(const Duration(milliseconds: 100));
+      await tester.pumpAndSettle();
 
       // find "Registration" text
       final regHeadingFinder = find.byKey(Key('rv_registration_heading'));
@@ -71,7 +93,56 @@ void _widget_tests() {
     });
   }); //group Text
   //TODO implement test
-  group('Email', () {}); //group Email
+  group('Email', () {
+    testWidgets('form field should render successfully.',
+        (WidgetTester tester) async {
+      final widget = Momentum(
+          child: MaterialApp(
+            home:
+                RegistrationVerificationPage(Key('registration_verification')),
+          ),
+          controllers: [
+            UserController(),
+          ]);
+      await tester.pumpWidget(widget);
+      await tester.pumpAndSettle();
+
+      // Find text form fields
+      final textFieldFinder = find.byKey(Key('email_input_field'));
+      // Expect 1 rendered text form field
+      expect(textFieldFinder, findsOneWidget);
+    });
+
+    testWidgets('form field should be empty and display an error string.',
+        (WidgetTester tester) async {
+      final widget = Momentum(
+          child: MaterialApp(
+            home:
+                RegistrationVerificationPage(Key('registration_verification')),
+          ),
+          controllers: [
+            UserController(),
+          ]);
+      await tester.pumpWidget(widget);
+      await tester.pumpAndSettle();
+
+      // find activaton code text form field
+      final codeFieldFinder = find.byKey(Key('code_input_field'));
+      //Attach keyboard to acquire focus
+      await tester.showKeyboard(codeFieldFinder);
+      //enter invalid text
+      await tester.enterText(codeFieldFinder, 'invalid code');
+      // find empty text
+      final textFinder = find.text('');
+      // Find error message
+      final textErrorFinder = find.text('* Required');
+      expect(find.byKey(Key('email_input_field')), findsOneWidget);
+      // Expect 1 empty text form fields
+      //expect(textFinder, findsOneWidget);
+      // Expect 1 error messages, one for each empty text form fields
+      //expect(textErrorFinder, findsOneWidget);
+    });
+  }); //group Email
   //TODO implement test
   group('Activation code', () {}); //group Activation code
   //TODO implement test
