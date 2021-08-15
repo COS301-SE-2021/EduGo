ac00f604170aaa4ec71b2123f571d16f9cac16ec
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-08-15 19:17:06 +0200

    Started switch to ar_flutter_plugin

---
+++ mobile/lib/src/Pages/VirtualEntityPage/View/ARWindow.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ARWindow extends StatefulWidget {
+  ARWindow({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => ARWindowState();
+
+}
+
+class ARWindowState extends State<ARWindow> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
