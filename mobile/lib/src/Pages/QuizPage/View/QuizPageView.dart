cb983aa0b2d5a95437e791906660342516932e4c
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-08-16 18:14:50 +0200

    bug: can't get virtual entity w/out disrupting momentum

--- mobile/lib/src/Pages/QuizPage/View/QuizPageView.dart
+++ mobile/lib/src/Pages/QuizPage/View/QuizPageView.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:mobile/src/Components/mobile_page_layout.dart';
 import 'package:mobile/src/Pages/QuizPage/Controller/QuestionController.dart';
 import 'package:mobile/src/Pages/QuizPage/Model/QuestionModel.dart';
+import 'package:mobile/src/Pages/VirtualEntityPage/Controller/VirtualEntityController.dart';
+import 'package:mobile/src/Pages/VirtualEntityPage/Models/VirtualEntityModels.dart';
+import 'package:http/http.dart' as http;
 import 'package:momentum/momentum.dart';
 
 class QuizView extends StatefulWidget {
+  //final VirtualEntityData data;
   QuizView({Key? key}) : super(key: key);
 
   @override
@@ -12,6 +16,72 @@ class QuizView extends StatefulWidget {
 }
 
 class _QuizViewState extends State<QuizView> {
+  //VirtualEntityData data;
+  //late Future<VirtualEntity> entity;
+
+  @override
+  void initState() {
+    super.initState();
+    //this.entity = getVirtualEntity(1, client: http.Client());
+    //this.entity = getVirtualEntity(data.ve_id, client: http.Client());
+  }
+
+  //_QuizViewState();
+
+  /*class Tech {
+  String label;
+  bool isSelected;
+  Tech(this.label, this.isSelected);
+}
+
+  //MC
+  bool selected = false;
+  List<Tech> _chipsList = [
+    Tech("Android", false),
+    Tech("Flutter", false),
+    Tech("Ios", false),
+    Tech("Python", false),
+    Tech("Go lang", false)
+  ];
+  //TOF
+  String? _value = '';
+  String type = 'MC'; //'MC'; //
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Choice Chips Demo',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Choice Chips Demo'),
+        ),
+        body: Column(
+          children: List<Widget>.generate(3, (int index) {
+            if (type == 'TOF') {
+              return ChoiceChip(
+                label: Text('Item $index'),
+                selected: _value == 'Item $index',
+                onSelected: (bool selected) {
+                  setState(() {
+                    _value = selected ? 'Item $index' : null;
+                  });
+                },
+              );
+            } else
+              return FilterChip(
+                  label: Text(_chipsList[index].label),
+                  labelStyle: TextStyle(color: Colors.white),
+                  backgroundColor: Colors.green,
+                  selected: _chipsList[index].isSelected,
+                  onSelected: (bool value) {
+                    setState(() {
+                      _chipsList[index].isSelected = value;
+                    });
+                  });
+          }).toList(),
+        ),
+      ),
+    );}*/
   @override
   Widget build(BuildContext context) {
     return MobilePageLayout(
