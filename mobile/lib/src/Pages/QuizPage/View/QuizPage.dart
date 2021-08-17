74de15df156f833bd897ef893d599378a376b471
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-08-17 16:34:57 +0200

    bug: api call

--- mobile/lib/src/Pages/QuizPage/View/QuizPage.dart
+++ mobile/lib/src/Pages/QuizPage/View/QuizPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/src/Components/mobile_page_layout.dart';
 import 'package:mobile/src/Pages/QuizPage/Controller/QuizController.dart';
+import 'package:mobile/src/Pages/QuizPage/Model/QuizModel.dart';
 import 'package:mobile/src/Pages/QuizPage/Model/QuizPageModel.dart';
 import 'package:momentum/momentum.dart';
 
@@ -15,8 +16,10 @@ class _QuizPageState extends State<QuizPage> {
   @override
   void initState() {
     super.initState();
+    int index = -1;
   }
 
+  int index = -1;
   @override
   Widget build(BuildContext context) {
     //   String? _value = '';
@@ -31,14 +34,26 @@ class _QuizPageState extends State<QuizPage> {
         false,
         false,
         MomentumBuilder(
+            // Quiz controller fetches JSON data of the Quiz
+            // Question controller displays a question at a time while maintaining state (has index and list if questions)
             controllers: [QuizController],
             builder: (context, snapshot) {
               //List of quizes
               final quizzes = snapshot<QuizPageModel>();
-              var allQuestionText = quizzes.quizes
-                  .map((e) => e.questions!.map((e) => print(e.question)));
-
-              return Row(children: [Text('no questions to display')]);
+              List<Question> questions = [];
+              //print('ek is hier'); makes it in then throws error
+              var allQuestions =
+                  quizzes.quizes.map((e) => e.questions!.map((e) {
+                        questions.add(e);
+                        print(e.toString());
+                      }));
+              if (index <= allQuestions.length) {
+                index++;
+                return Row(children: [
+                  //Text(questions.elementAt(index).question)
+                ]); //return Row(children: [Text(index.toString())]);
+              }
+              return Row(children: [Text('no quiz to display')]);
             }),
         'Quiz');
   }
