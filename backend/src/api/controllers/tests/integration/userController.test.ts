9a0108462701915f623412c9297541baab96309e
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-08-19 08:28:33 +0200

    Added tests to the Auth Controller

---
+++ backend/src/api/controllers/tests/integration/userController.test.ts
@@ -0,0 +1,7 @@
+import 'reflect-metadata';
+	describe("Organisation tests ", () => {
+		it("First test login ", async () => {
+            expect(true).toBe(true)
+		
+		});
+	});
---
+++ backend/src/api/controllers/tests/unit/SubjectController.test.ts
@@ -0,0 +1,7 @@
+import 'reflect-metadata';
+	describe("Subject tests ", () => {
+		it("First test login ", async () => {
+            expect(true).toBe(true)
+		
+		});
+	});
