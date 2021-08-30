478ec0db33e5024f3905738adc000b7f58993dda
Noah(u19181851) <u19181851@tuks.co.za>
2021-08-30 18:08:16 +0200

    create lesson error functionality implemented

--- edugo_web_app/lib/src/Pages/CreateLesson/View/Components/CreateLessonContainer.dart
+++ edugo_web_app/lib/src/Pages/CreateLesson/View/Components/CreateLessonContainer.dart
@@ -1,7 +1,19 @@
 import 'package:edugo_web_app/src/Pages/CreateLesson/View/Widgets/CreateLessonWidgets.dart';
 import 'package:edugo_web_app/src/Pages/EduGo.dart';
 
-class CreateLessonContainer extends StatelessWidget {
+class CreateLessonContainer extends StatefulWidget {
+  final GlobalKey<FormState> createLessonParentKey;
+  final Function containerOnSubmit;
+
+  const CreateLessonContainer(
+      {Key key, this.createLessonParentKey, this.containerOnSubmit})
+      : super(key: key);
+
+  @override
+  _CreateLessonContainerState createState() => _CreateLessonContainerState();
+}
+
+class _CreateLessonContainerState extends State<CreateLessonContainer> {
   @override
   Widget build(BuildContext context) {
     return MomentumBuilder(
@@ -17,9 +29,14 @@ class CreateLessonContainer extends StatelessWidget {
               elevation: 40,
               borderRadius: BorderRadius.circular(10),
               child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                padding:
+                    EdgeInsets.only(top: 25, bottom: 5, right: 20, left: 20),
                 child: CreateLessonInputBox(
                   text: "Lesson title...",
+                  createLessonKey: widget.createLessonParentKey,
+                  onSubmit: () {
+                    widget.containerOnSubmit();
+                  },
                   onChanged: (lessonTitle) {
                     Momentum.controller<CreateLessonController>(context)
                         .setLessonTitle(lessonTitle);
@@ -37,6 +54,10 @@ class CreateLessonContainer extends StatelessWidget {
                 padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                 child: CreateLessonMultiLine(
                   text: "Lesson description...",
+                  createLessonKey: widget.createLessonParentKey,
+                  onSubmit: () {
+                    widget.containerOnSubmit();
+                  },
                   onChanged: (lessonDescription) {
                     Momentum.controller<CreateLessonController>(context)
                         .setLessonDescription(lessonDescription);
--- edugo_web_app/lib/src/Pages/CreateLesson/View/Widgets/CreateLessonInputBox.dart
+++ edugo_web_app/lib/src/Pages/CreateLesson/View/Widgets/CreateLessonInputBox.dart
@@ -4,17 +4,29 @@ import 'package:flutter/material.dart';
 class CreateLessonInputBox extends StatelessWidget {
   final String text;
   final Function onChanged;
-  const CreateLessonInputBox({Key key, this.text, this.onChanged})
+  final Function onSubmit;
+  final GlobalKey<FormState> createLessonKey;
+  const CreateLessonInputBox(
+      {Key key, this.text, this.onChanged, this.onSubmit, this.createLessonKey})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: ScreenUtil().setWidth(1000),
-      height: 60,
-      child: TextField(
+      height: 75,
+      child: TextFormField(
         onChanged: (value) {
           onChanged(value);
+        },
+        validator: (value) {
+          if (value.isEmpty) {
+            return 'Required field, cannot be blank';
+          }
+          return null;
+        },
+        onFieldSubmitted: (value) {
+          if (createLessonKey.currentState.validate()) onSubmit();
         },
         cursorColor: Color.fromARGB(255, 97, 211, 87),
         decoration: InputDecoration(
