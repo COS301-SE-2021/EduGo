534b517e09c72bd404db839f8aded20c58cb0a4a
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-08-31 22:24:51 +0200

    started adding url and request support to web

--- canvas/web/src/app.ts
+++ canvas/web/src/app.ts
@@ -4,6 +4,8 @@ import * as gltf from 'webgl-gltf';
 import * as camera from './camera';
 import * as renderer from './renderer';
 import * as inputs from './inputs';
+// import * as request from './requests';
+// import axios from 'axios';
 
 const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
 const gl: WebGLRenderingContext = canvas.getContext('webgl')!;
@@ -34,6 +36,18 @@ const zoom = (delta: number) => {
 inputs.listen(canvas, rotate, zoom);
 
 if (!gl) alert('WebGL not available');
+
+// const pre = async () => {
+//     let modelId = request.getParameter('model');
+//     if (!modelId) throw new Error('Missing model id in parameters');
+    
+
+//     let response = await axios.post('http://localhost:8084/api/model', {model: parseInt(modelId)});
+//     if (response.status === 200) {
+//         let url = response.data.url;
+//         console.log(url);
+//     }
+// }
 
 const init = async () => {
     gl.clearColor(0.3, 0.3, 0.3, 1.0);
@@ -77,7 +91,7 @@ const init = async () => {
     const uniforms = defaultShaders.getUniformLocations(gl, program);
 
     const environment = await cubemap.load(gl);
-    const model = await gltf.loadModel(gl, `models/waterbottle/waterbottle.gltf`);
+    const model = await gltf.loadModel(gl, `https://edugo-files.s3.af-south-1.amazonaws.com/test_models/waterbottle/waterbottle.gltf`);
 
     cubemap.bind(gl, environment, uniforms.brdfLut, uniforms.environmentDiffuse, uniforms.environmentSpecular);
 
