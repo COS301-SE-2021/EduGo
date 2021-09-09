d03eded63ce918337fabac681e39f9ec6a0203fb
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-09-09 19:14:33 +0200

    Modified lesson tests

--- backend/src/api/middleware/ValidationMiddleware.ts
+++ backend/src/api/middleware/ValidationMiddleware.ts
@@ -113,7 +113,7 @@ export class IsEducatorMiddleware implements ExpressMiddlewareInterface {
 		if (req.headers.authorization) {
 			const token = req.headers.authorization.slice(7);
 			const payload = jwtDecode<MyPayload>(token);
-
+			console.log(payload);
 			try {
 				let user: AuthenticateObject = await this.validationMiddleware.getUserDetails(
 					payload.user_id
