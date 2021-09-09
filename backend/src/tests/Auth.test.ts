8c800eaf35e9e31e7adbca189aceb5693d9407ba
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-09-09 14:09:06 +0200

    Completed login api test

--- backend/src/tests/Auth.test.ts
+++ backend/src/tests/Auth.test.ts
@@ -3,7 +3,6 @@ import * as App from './App';
 import request from 'supertest';
 import * as Default from './Default';
 import { anything, when } from 'ts-mockito';
-import { User } from '../api/database/User';
 
 describe('Auth API tests', () => {
     describe('POST /auth/login', () => {
@@ -18,9 +17,167 @@ describe('Auth API tests', () => {
             const response = await request(App.app)
                 .post('/auth/login')
                 .set('Accept', 'application/json')
-                .send(req);
+                .send(req)
+                .expect(200)
+                .expect('Content-Type', /json/);
 
-            console.log(response.body);
-        })
-    })
+            expect(response.body.token).toBeDefined();
+        });
+
+        it('should successfully login an educator and return a token', async () => {
+            const req: LoginRequest = {
+                username: 'educator',
+                password: 'password'
+            }
+
+            when(App.mockedUserRepository.findOne(anything())).thenResolve(Default.educatorUser);
+
+            const response = await request(App.app)
+                .post('/auth/login')
+                .set('Accept', 'application/json')
+                .send(req)
+                .expect(200)
+                .expect('Content-Type', /json/);
+
+            expect(response.body.token).toBeDefined();
+        });
+
+        it('should successfully login an admin and return a token, async', async () => {
+            const req: LoginRequest = {
+                username: 'admin',
+                password: 'password'
+            }
+
+            when(App.mockedUserRepository.findOne(anything())).thenResolve(Default.adminUser);
+
+            const response = await request(App.app)
+                .post('/auth/login')
+                .set('Accept', 'application/json')
+                .send(req)
+                .expect(200)
+                .expect('Content-Type', /json/);
+
+            expect(response.body.token).toBeDefined();
+        });
+
+        it('should return an Unauthorized error if a student username is not found and a message stating the username was nout found', async () => {
+            const req: LoginRequest = {
+                username: 'student',
+                password: 'password'
+            }
+
+            when(App.mockedUserRepository.findOne(anything())).thenResolve(undefined);
+
+            const response = await request(App.app)
+                .post('/auth/login')
+                .set('Accept', 'application/json')
+                .send(req)
+                .expect(401)
+                .expect('Content-Type', /json/);
+
+            expect(response.body.message).toBeDefined();
+            expect(response.body.message).toContain('username');
+            expect(response.body.message).toContain('not found');
+        });
+
+        it('should return an Unauthorized error if a student password is incorrect and a message stating the password was incorrect', async () => {
+            const req: LoginRequest = {
+                username: 'student',
+                password: 'wrongpassword'
+            }
+
+            when(App.mockedUserRepository.findOne(anything())).thenResolve(Default.studentUser);
+
+            const response = await request(App.app)
+                .post('/auth/login')
+                .set('Accept', 'application/json')
+                .send(req)
+                .expect(401)
+                .expect('Content-Type', /json/);
+
+            expect(response.body.message).toBeDefined();
+            expect(response.body.message).toContain('assword');
+            expect(response.body.message).toContain('ncorrect');
+        });
+
+        it('should return an Unauthorized error if an educator username is not found and a message stating the username was nout found', async () => {
+            const req: LoginRequest = {
+                username: 'educator',
+                password: 'password'
+            }
+
+            when(App.mockedUserRepository.findOne(anything())).thenResolve(undefined);
+
+            const response = await request(App.app)
+                .post('/auth/login')
+                .set('Accept', 'application/json')
+                .send(req)
+                .expect(401)
+                .expect('Content-Type', /json/);
+
+            expect(response.body.message).toBeDefined();
+            expect(response.body.message).toContain('username');
+            expect(response.body.message).toContain('not found');
+        });
+
+        it('should return an Unauthorized error if an educator password is incorrect and a message stating the password was incorrect', async () => {
+            const req: LoginRequest = {
+                username: 'educator',
+                password: 'wrongpassword'
+            }
+
+            when(App.mockedUserRepository.findOne(anything())).thenResolve(Default.educatorUser);
+
+            const response = await request(App.app)
+                .post('/auth/login')
+                .set('Accept', 'application/json')
+                .send(req)
+                .expect(401)
+                .expect('Content-Type', /json/);
+
+            expect(response.body.message).toBeDefined();
+            expect(response.body.message).toContain('assword');
+            expect(response.body.message).toContain('ncorrect');
+        });
+
+        it('should return an Unauthorized error if an admin username is not found and a message stating the username was nout found', async () => {
+            const req: LoginRequest = {
+                username: 'admin',
+                password: 'password'
+            }
+
+            when(App.mockedUserRepository.findOne(anything())).thenResolve(undefined);
+
+            const response = await request(App.app)
+                .post('/auth/login')
+                .set('Accept', 'application/json')
+                .send(req)
+                .expect(401)
+                .expect('Content-Type', /json/);
+
+            expect(response.body.message).toBeDefined();
+            expect(response.body.message).toContain('username');
+            expect(response.body.message).toContain('not found');
+        });
+
+        it('should return an Unauthorized error if an admin password is incorrect and a message stating the password was incorrect', async () => {
+            const req: LoginRequest = {
+                username: 'admin',
+                password: 'wrongpassword'
+            }
+
+            when(App.mockedUserRepository.findOne(anything())).thenResolve(Default.adminUser);
+
+            const response = await request(App.app)
+                .post('/auth/login')
+                .set('Accept', 'application/json')
+                .send(req)
+                .expect(401)
+                .expect('Content-Type', /json/);
+
+            expect(response.body.message).toBeDefined();
+            expect(response.body.message).toContain('assword');
+            expect(response.body.message).toContain('ncorrect');
+        });
+    });
 })
