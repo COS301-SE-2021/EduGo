b0418c7488f6856e929a68e9f346f0afc5106e14
kieran-woodrow <77970893+kieran-woodrow@users.noreply.github.com>
2021-09-12 14:28:22 +0200

    Updated virtual entity model

--- mobile/lib/src/Pages/VirtualEntityPage/Models/VirtualEntityModels.dart
+++ mobile/lib/src/Pages/VirtualEntityPage/Models/VirtualEntityModels.dart
@@ -1,10 +1,17 @@
-import 'package:json_annotation/json_annotation.dart';
+/**
+ * This is the model for a virtual entity. 
+ * The getVirtualEntity endpoint will return a the information 
+ * with regards to the specific virtual entity id that is passed in. 
+ */
 
+import 'package:json_annotation/json_annotation.dart';
 part 'VirtualEntityModels.g.dart';
 
 class VirtualEntityData {
+  //Holds the virtual enitity id
   final int ve_id;
 
+  //Virtual entity data constructor
   VirtualEntityData({required this.ve_id});
 
   factory VirtualEntityData.fromJson(Map<String, dynamic> json) {
@@ -14,22 +21,29 @@ class VirtualEntityData {
 
 @JsonSerializable(explicitToJson: true)
 class VirtualEntity {
+  //Holds the virtual entity ID
   @JsonKey(required: true)
   int id;
 
+  //Holds the virtual entity title
   @JsonKey(required: true)
   String title;
 
+  //Holds the virtual entity description
   @JsonKey(defaultValue: '')
   String description;
 
+  //Holds the virtual entity description
   @JsonKey(defaultValue: [])
   List<String> information;
 
+  //Holds the actual model
   @JsonKey(defaultValue: null)
   Model? model;
 
-  VirtualEntity(this.id, this.title, this.description, this.information, this.model?);
+  //Virtual entity constructor
+  VirtualEntity(
+      this.id, this.title, this.description, this.information, this.model);
 
   factory VirtualEntity.fromJson(Map<String, dynamic> json) =>
       _$VirtualEntityFromJson(json);
@@ -38,13 +52,15 @@ class VirtualEntity {
 
 @JsonSerializable()
 class Model {
+  //Holds the filelink
   @JsonKey(required: true)
   String fileLink;
 
+  //Holds a thumbnail of the 3d model
   @JsonKey(required: true)
   String thumbnail;
 
-
+  //Model consatructor
   Model(this.fileLink, this.thumbnail);
 
   factory Model.fromJson(Map<String, dynamic> json) => _$ModelFromJson(json);
