5be1fbfe2a9b7907e79b219294fd380af40c3b9c
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-09-13 21:42:10 +0200

    Initialised converter service

---
+++ converter/src/index.ts
@@ -0,0 +1,6 @@
+import { platform } from 'os';
+import { spawnSync } from 'child_process';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
