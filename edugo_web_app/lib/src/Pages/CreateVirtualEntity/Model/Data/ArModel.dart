fd26f246b2a24a702d52272e1a4d86f3e50d3a9a
Noah(u19181851) <u19181851@tuks.co.za>
2021-09-13 08:11:40 +0200

    ArModel upload , updated

--- edugo_web_app/lib/src/Pages/CreateVirtualEntity/Model/Data/ArModel.dart
+++ edugo_web_app/lib/src/Pages/CreateVirtualEntity/Model/Data/ArModel.dart
@@ -1,59 +1,19 @@
 class ArModel {
-  String _name;
-  String _description;
-  String _file_link;
-  int _file_size;
-  String _file_type;
-  String _file_name;
-  String _preview_img;
+  String _fileLink;
+  String _previewImg;
 
-  ArModel(
-      {name,
-      description,
-      file_name,
-      file_link,
-      file_size,
-      file_type,
-      preview_img})
-      : _description = description,
-        _file_name = file_name,
-        _name = name,
-        _file_link = file_link,
-        _file_size = file_size,
-        _file_type = file_type,
-        _preview_img = preview_img;
-
-  void setName(String name) {
-    _name = name;
-  }
-
-  void setFileName(String name) {
-    _file_name = name;
-  }
-
-  void setDescription(String description) {
-    _description = description;
-  }
+  ArModel({fileLink, previewImg})
+      : _fileLink = fileLink,
+        _previewImg = previewImg;
 
   void setFileLink(String link) {
-    _file_link = link;
+    _fileLink = link;
   }
 
-  void setFileSize(int size) {
-    _file_size = size;
+  void setPreviewImage(String imageLink) {
+    _previewImg = imageLink;
   }
 
-  void setFileType(String type) {
-    _file_type = type;
-  }
-
-  Map<String, dynamic> toJson() => {
-        "name": _name,
-        "description": _description,
-        "file_link": _file_link,
-        "file_size": _file_size,
-        "file_type": _file_type,
-        "file_name": _file_name,
-        "preview_img": ""
-      };
+  Map<String, dynamic> toJson() =>
+      {"fileLink": _fileLink, "thumbnail": _previewImg};
 }
