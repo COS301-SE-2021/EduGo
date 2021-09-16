709f2a58d4fa318b00fb57ed18b5058e47266b33
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2021-09-16 20:50:56 +0200

    feat: update info page

--- mobile/lib/src/Pages/LessonsPage/Controller/LessonInformationController.dart
+++ mobile/lib/src/Pages/LessonsPage/Controller/LessonInformationController.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/src/Pages/DetectMarkerPage/View/DetectMarkerPage.dart';
 import 'package:mobile/src/Pages/LessonsPage/Models/LessonInformationModel.dart';
 import 'package:mobile/src/Pages/LessonsPage/View/LessonInformationPage.dart';
 import 'package:mobile/src/Pages/QuizPage/Controller/QuizzesPageController.dart';
@@ -38,6 +39,44 @@ class LessonInformationController
         }
       },
     );
+  }
+
+  // Load thumbnails
+  List<Widget> _mockImages(
+      int noOfThumbnails, List<VirtualEntity> lessonVirtualEntities) {
+    List<Widget> imagesAndCaptions = [];
+
+    for (int i = 0; i < noOfThumbnails; i++) {
+      imagesAndCaptions.add(
+        Expanded(
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            Material(
+              shape: CircleBorder(),
+              elevation: 3.0,
+              child: Image.network(
+                //Mobile app: child: Image.asset(
+                lessonVirtualEntities.elementAt(i).model!.thumbnail,
+                fit: BoxFit.fill,
+                height: 80,
+                width: 100,
+              ),
+            ),
+            Expanded(
+              child: FittedBox(
+                child: Text(
+                  'id: ' + lessonVirtualEntities.elementAt(i).id.toString(),
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      );
+    }
+    return imagesAndCaptions;
   }
 
   Widget getView(context, String lessonTitle, String lessonDescription,
@@ -102,6 +141,75 @@ class LessonInformationController
                         ),
                       ),
                     ]),
+                //Lesson description
+                new Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 25),
+                    child: Text(
+                      lessonDescription,
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 10,
+                      softWrap: true,
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                  ),
+                ),
+                //Scan QR code
+                Padding(
+                  padding: const EdgeInsets.only(top: 15.0, bottom: 8.0),
+                  child: MaterialButton(
+                      elevation: 20,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {
+                        MomentumRouter.goto(
+                          context,
+                          DetectMarkerPage,
+                          transition: (context, page) {
+                            return MaterialPageRoute(
+                                builder: (context) => page);
+                          },
+                        );
+                      },
+                      minWidth: 10,
+                      height: 25,
+                      color: Color.fromARGB(255, 97, 211, 87),
+                      disabledColor: Color.fromRGBO(211, 212, 217, 1),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.qr_code,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                            Text(
+                              "Scan QR code ",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      )),
+                ),
+
+                //Virtual entities display:
+                //the number of tabs represent the no of virual entity
+                // each virtual entity is labeled by it's id
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 3,
+                  children: _mockImages(
+                      lessonVirtualEntities.length, lessonVirtualEntities),
+                ),
               ],
             ),
           ]),
