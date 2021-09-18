0f7ac587232bee9bcf0ab4a9d84f163f279d7b15
simekani18 <82416346+simekani18@users.noreply.github.com>
2021-09-18 20:12:24 +0200

    Subject tests done

--- backend/src/api/services/SubjectService.ts
+++ backend/src/api/services/SubjectService.ts
@@ -108,7 +108,6 @@ export class SubjectService {
 			throw new BadRequestError("Could not find error");
 		}
 
-		console.log(user);
 
 		if (!user) throw new BadRequestError("Could not find user");
 
